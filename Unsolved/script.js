22c89f110ff41afe3c9c02a4aeee201a200061b7
shweyeetheint <shweyeetheint690@gmail.com>
2024-05-25 21:15:31 +0630

    first

---
+++ Unsolved/script.js
@@ -0,0 +1,19 @@
+// TODO: Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = x < 25;
+var expression2 = x > 50;
+console.log(x<25);
+
+// TODO: Write Your JavaScript Code Here
+if (experssion1 && experssion2 ) {
+    console.log ('T T')
+};
+else if (experssion1) {
+    console.log ('T F');
+}
+else if (experssion2) {
+    console.log ('F T');
+}
+else {
+    console.log ('F F');
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+var y = 100;
+var z = y < 88;
+var c = y > 100;
+
+if (z && z) {
+    console.log ("T T");
+
+}
+else if (z) {
+    console.log ("T F");
+}
+
+else if (c) {
+    console.log ("F T");
+
+}
+else {
+    console.log ("F F");
+}
